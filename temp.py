65ab9b98cbb4a37ab00f134efe33bde6f941bd1c
Allison2487 <Allisonleung75@gmail.com>
2025-10-09 10:05:53 -0400

    Tried Plotting

--- temp.py
+++ temp.py
@@ -1,2 +1,9 @@
 # Final project
+import pandas as pd
+import matplotlib.pyplot as plt
 
+data = pd.read_csv("data.csv")
+
+print(data["Number_with_outcome"])
+
+plt.hist(data["Topic", "Number_with_outcome"].head(100))
