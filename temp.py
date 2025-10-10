0602e7487e1725a5660fa53301ceb9631d6d3cd3
Archanna08 <archannanagesan@gmail.com>
2025-10-10 16:15:38 -0400

    assisted ventilatilation dataset filtration
    tried something... cant tell if it's working

--- temp.py
+++ temp.py
@@ -7,3 +7,8 @@ data = pd.read_csv("data.csv")
 print(data["Number_with_outcome"])
 
 plt.hist(data["Topic", "Number_with_outcome"].head(100))
+
+
+# assisted ventilation
+for i in range(17,27):
+    print(data.iloc[i]['Age group'],data.iloc[i]['Year'], data.iloc[i]['Denominator'])
