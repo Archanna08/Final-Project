115a05b0f8bb2aa7b86cb043e4db85d4449447e6
Allison2487 <Allisonleung75@gmail.com>
2025-11-13 22:42:14 -0500

    plotting and descriptions

--- True Del3.py
+++ True Del3.py
@@ -12,14 +12,15 @@ import seaborn as sns
 Raw_data=pd.read_csv("data.csv")
 
 #2. Sorting, Filtering and Identifying Data
-pd.DataFrame.info(Raw_data)
-pd.DataFrame.describe(Raw_data)
-
 Used_Data= Raw_data[["Age_group", "Health_condition-Data_source", "Sex", "Year", "Denominator", "Number_with_outcome", "Insurance", "Primary_Neighborhood", "Rate_SF_pop", "Race_ethnicity"]]
+pd.DataFrame.info(Used_Data)
+pd.DataFrame.describe(Used_Data)
+pd.DataFrame.nunique(Used_Data)
+print(Used_Data["Year"].value_counts())
+
 pd.DataFrame.duplicated(Used_Data).value_counts()
 Used_Data=pd.DataFrame.drop_duplicates(Used_Data)
 
-    #Overall filtered dataset that we're gonna use
 Used_Data=pd.DataFrame(Used_Data).dropna(subset=["Year"]) #dropna(subset) drops rows with null value in a certain column (subset)
 print(pd.DataFrame.isnull(Used_Data["Year"]).sum()) #To check if the rows were dropped
 
@@ -53,12 +54,12 @@ print(Used_Data[variable_cat].describe(include='object'))
        #top=mode, freq count=count
        
 #4. Univariate Graphical EDA
-for i in variable_num:
-    sns.displot(data=G_Pub_Pri_Insurance, x=i, multiple="dodge", hue="Health_condition-Data_source", bins=35)
+for i in variable_num: #Not all the graphs generated here are used
+    sns.displot(data=G_HIV_tests, x=i, multiple="dodge", hue="Sex", bins=35)
     sns.displot(data=G_HIV_tests, x=i, multiple="stack", hue="Age_group")
-    sns.displot(data=G_Depression_admissions, x=i, kind="kde", hue="Year", bw_adjust=.5)
-    sns.displot(data=G_All_ER_visits, x=i, hue="Year", element="step", stat='density')
-    sns.displot(data=G_Authorized_meds, x=i, kind="ecdf")
+    sns.displot(data=G_HIV_tests, x=i, kind="kde", hue="Year", bw_adjust=.5)
+    sns.displot(data=G_HIV_tests, x=i, hue="Year", element="step", stat='density')
+    sns.displot(data=G_HIV_tests, x=i, hue="Sex", kind="ecdf")
 
 #5. Multivariate Non-Graphical EDA ?????? Don't know what this is gonna tell us :)
 pd.crosstab(Data_Neighborhood["Primary_Neighborhood"], Data_Neighborhood["Sex"])
@@ -83,7 +84,7 @@ sns.relplot(data= G_Alcohol, x='Denominator', y='Number_with_outcome', kind='lin
 #e
 sns.lmplot(data = G_Pub_Pri_Insurance , x= 'Denominator', y='Number_with_outcome', hue='Health_condition-Data_source').set(title='Number of Pregnant Women with Different Insurances from CDPH Birth Records')
 
-    #6.2 Categorical Raw_data
+    #6.2 Categorical data
 #a #spacing
 sns.catplot(data=G_Timely_med_exam , x= 'Year', y='Rate_SF_pop', jitter= False,  height=4,aspect=4 ).set(title='Percentage of Timely Medical Exam from 1998-2023')
 
